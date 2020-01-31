f24400267bee89c70185813315857c86ad49d9a7
MinexAutomation <minex.automation@gmail.com>
2020-01-30 17:34:35 -0800

    Added IServiceCollection extensions.

---
+++ source/R5T.Solutas.Megara/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Cambridge.Types;
+using R5T.Dacia;
+using R5T.Megara;
+
+
+namespace R5T.Solutas.Megara
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="VisualStudioSolutionFileSerializer"/> implementation of <see cref="IVisualStudioSolutionFileSerializer"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceCollection AddMegaraVisualStudioSolutionFileSerializer(this IServiceCollection services,
+            ServiceAction<IFileSerializer<SolutionFile>> addSolutionFileSerializer)
+        {
+            services
+                .AddSingleton<IVisualStudioSolutionFileSerializer, VisualStudioSolutionFileSerializer>()
+                .RunServiceAction(addSolutionFileSerializer)
+                ;
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="VisualStudioSolutionFileSerializer"/> implementation of <see cref="IVisualStudioSolutionFileSerializer"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static ServiceAction<IVisualStudioSolutionFileSerializer> AddMegaraVisualStudioSolutionFileSerializerAction(this IServiceCollection services,
+            ServiceAction<IFileSerializer<SolutionFile>> addSolutionFileSerializer)
+        {
+            var serviceAction = new ServiceAction<IVisualStudioSolutionFileSerializer>(() => services.AddMegaraVisualStudioSolutionFileSerializer(
+                addSolutionFileSerializer));
+            return serviceAction;
+        }
+    }
+}
