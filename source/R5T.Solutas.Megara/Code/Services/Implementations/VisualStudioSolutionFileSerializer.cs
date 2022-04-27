d20406c66e14d56d7449bcf644ad703f636b82f7
davidcoats <david@getsafetycone.com>
2022-04-27 15:35:34 -0700

    Interim changes.

--- source/R5T.Solutas.Megara/Code/Services/Implementations/VisualStudioSolutionFileSerializer.cs
+++ source/R5T.Solutas.Megara/Code/Services/Implementations/VisualStudioSolutionFileSerializer.cs
@@ -1,13 +1,13 @@
-﻿using System;
+using System;
 
 using R5T.Cambridge.Types;
 using R5T.Megara;
-using R5T.Megara.Default;
+using R5T.Megara.Default;using R5T.T0064;
 
 
 namespace R5T.Solutas.Megara
-{
-    public class VisualStudioSolutionFileSerializer : FileSerializerBasedFileSerializer<SolutionFile>, IVisualStudioSolutionFileSerializer
+{[ServiceImplementationMarker]
+    public class VisualStudioSolutionFileSerializer : FileSerializerBasedFileSerializer<SolutionFile>, IVisualStudioSolutionFileSerializer,IServiceImplementation
     {
         public VisualStudioSolutionFileSerializer(IFileSerializer<SolutionFile> fileSerializer)
             : base(fileSerializer)
